1a77d0e818e4611c9d16786bf59a5eaceb40324c
targed <ltklionel@gmail.com>
2026-06-19 10:19:57 -0500

    experiment: 6_5_in_4 baseline

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ from datetime import datetime
 # CONFIG: edit this section to choose/switch problems
 # ---------------------------------------------------------------------------
 
-CURRENT_PROBLEM = "8_4_in_6"
+CURRENT_PROBLEM = "6_5_in_4"
 
 TIME_BUDGET = 300
 
